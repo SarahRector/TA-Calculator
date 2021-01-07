5e6da56d95748f3cde1b003693d329fbeb6fac73
SarahRector <rector.sarah.r@gmail.com>
2021-01-07 11:28:55 -0800

    added addition

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // import functions and grab DOM elements
+const additionField = document.getElementById('add-input-1');
+const additionField2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addResults = document.getElementById('add-results');
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+addButton.addEventListener('click', () => {
+    const fieldValue = additionField.valueAsNumber;
+    const fieldValue2 = additionField2.valueAsNumber;
+
+    const sum = fieldValue + fieldValue2;
+
+    addResults.textContent = sum;
+});
